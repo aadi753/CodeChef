82c696b1395a339eedc0d9395d3ce2a2f819aed3
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-09-05 17:49:26 +0530

    05-09-22 Practice
    L2 - adv

--- DIF_GCD.cpp
+++ DIF_GCD.cpp
@@ -26,6 +26,40 @@ int main(){
 		//check the maximum multiple of n that lies below =m
 		//then go to n + 1 and do the same while(a * x <= m)
 
+		long i = n;
+		long j = i;
+		long x;
+
+		long max = -1;
+		long long curr = -1;
+
+		while(i < (2 * n)){
+			//break when diff < max
+			//check for the higest multiple of i
+			//x = m / i;
+			//j = x * i;
+			//diff = j - i;
+			//in worst case it will be i = 5 and x = 6 / 5 = 1 and j = i * x;
+			//8 9 10 11 12 13 14 15 16 17
+			x = m / i;
+			j = x * i;
+
+			curr = j - i;
+
+// 			if(curr < max){
+// 				break;
+// 			}
+
+			if(curr > max){
+			    max = curr;
+			    a = i;
+			    b = j;
+			}
+		
+			i++;
+		}	
+
+		cout << a << ' ' << b << "\n";
 		
 	}
 	return 0;
